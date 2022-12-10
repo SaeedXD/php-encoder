56fb90d8c24d60fa3b558e2e558f5068f9837ecf
Saeed Golestani <me@saeedgolestani.ir>
2022-12-10 05:43:30 +0330

    some change to fix deploy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>PHP Encoder</title>
         <link rel="stylesheet" href="/assets/fonts/autoLoad.php">
-        @vite('resources/css/app.css')
-        @vite('resources/js/app.js')
+
+        {{-- @vite('resources/css/app.css') --}}
+        {{-- @vite('resources/js/app.js') --}}
+
+        <link rel="stylesheet" href="/build/assets/app.d12883f2.css">
         <script src='/assets/scripts/awesome.min.js' crossorigin='anonymous'></script>
     </head>
-    <body id="app" class="flex items-center flex-col gap-7 py-7 text-white/90">
-        <App />
+    <body >
+        <div id="app" class="w-screen h-screen overflow-y-auto flex items-center flex-col gap-7 py-7 text-white/90">
+            <App />
+        </div>
+        <script src='/build/assets/app.2f96d1a9.js'></script>
     </body>
 </html>
